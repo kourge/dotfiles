ce21bd6e8407b408bffa87943ab2363b0b221099
Wilson Lee <wlee@twitter.com>
2012-07-12 09:22:15 -0700

    Fix screen's broken feature

--- .local.bash
+++ .local.bash
@@ -1,3 +1,4 @@
+set -o vi
 
 export LC_CTYPE=en_US.UTF-8
 export CLICOLOR=1
@@ -25,4 +26,11 @@ export PS1=$_USER'@'$_HOST':'$_DIR$_GIT'\n'$_WHITE'  \$ '$_RESET
 alias be='bundle exec'
 alias git-branch='__git_ps1 "%s\n"'
 alias gb='git-branch'
+alias screen='export SCREENPWD=$(pwd); /usr/bin/env screen'
+
+case "$TERM" in
+  'screen')
+    cd $SCREENPWD
+    ;;
+esac
 
